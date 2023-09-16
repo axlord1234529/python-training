30430072d52ddf2740012d49eb13188594343725
Sándor Kalmár <66329053+axlord1234529@users.noreply.github.com>
2023-09-16 17:50:07 +0200

    set up db

--- django/display/models.py
+++ django/display/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Edge(models.Model):
+    id1 = models.IntegerField()
+    id2 = models.IntegerField()
+    weight = models.IntegerField()
+
+    def __str__(self):
+        return f"Edge {self.id1} - {self.id2}"
